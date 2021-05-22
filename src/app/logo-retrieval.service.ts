2629ae84021b01acb25cc47e4320de036c96bd87
tim-van-dyck-fh-campus-wien <tim.van.dyck@stud.fh-campuswien.ac.at>
2021-05-22 06:53:43 +0200

    Really fixed searchbar

--- src/app/logo-retrieval.service.ts
+++ src/app/logo-retrieval.service.ts
@@ -8,12 +8,6 @@ export class LogoRetrievalService {
 
   constructor(private http:HttpClient) { }
   public getLogoFromUrl(url:string):string{
-    /*let params = new HttpParams().set('url',"www.google.at");
-   
-    this.http.get("https://besticon-demo.herokuapp.com/allicons.json",{params:params}).subscribe((data) => {
-      console.dir(data);
-    })*/
-
     return "https://besticon-demo.herokuapp.com/icon?url="+url+"&size=80..120..200";
   }
 }
