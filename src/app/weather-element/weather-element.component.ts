5a56a9d3ffc2ecf688323b96dd9f300788e0f04a
theresanother1 <71547484+theresanother1@users.noreply.github.com>
2021-06-28 15:58:26 +0200

    minor fixes + code cleanup

--- src/app/weather-element/weather-element.component.ts
+++ src/app/weather-element/weather-element.component.ts
@@ -9,6 +9,7 @@ import { WeatherService } from '../weather.service';
 })
 export class WeatherElementComponent implements OnInit {
 
+  //initialize weatherData that is updated by WeatherService & used at other places too
 weatherData:WeatherData = {
   initialCity: "",
   city: "", 
@@ -20,20 +21,12 @@ weatherData:WeatherData = {
   activitiesWeatherDescription: "none",
 }
 
-//initialize data that is updated by WeatherService
-/*city: any;
-temp:number = 0;
-weatherID:number=0;
-weatherDescription: string = 'none';
-temp_min: number;
-temp_max: number;
-activitiesWeatherDescription: string;*/
 
-//error handling
+//error handling (only for this component)
 failed: boolean = false;
 loading: boolean = false;
 
-//change initial city
+//change initial city in DB
 textfieldIsVisible:boolean;
 
 
@@ -45,13 +38,11 @@ textfieldIsVisible:boolean;
    this.textfieldIsVisible = false;
   }
 
-   //makes a new API call for city entered
+   //new API call for city entered
   clickme(myCity:string){
     if (myCity == ""){
       alert("Please enter a city value.")
-    } 
-   // else if (this.city != myCity){
-      else if (this.weatherData.city != myCity){
+    } else if (this.weatherData.city != myCity) {
     this.loading = true;
     this.makeAPIcall(myCity);
     }
@@ -62,18 +53,13 @@ textfieldIsVisible:boolean;
     this.weatherService.getInitialCityFromDB().then((response)=>{
       this.weatherData.city = response;
       this.weatherData.initialCity = response;
-     // this.city=response;
-      //console.log(this.city);
       this.makeAPIcall(this.weatherData.city);
-      //this.makeAPIcall(this.city);
     })
   }
 
-   //takes care of API call itself
+   //updates weatherData with data from API call
   async makeAPIcall(myCity){
     this.weatherData.city = myCity; 
-    //let newData = (await this.weatherService.getCurrentWeather(this.city)).subscribe
-
     let newData = (await this.weatherService.getCurrentWeather(this.weatherData.city)).subscribe
     (x => {
      this.failed = false;
@@ -82,51 +68,24 @@ textfieldIsVisible:boolean;
      this.weatherData.weatherID = x.weather.id;
      this.weatherData.temp_min = x.temp_min.toFixed(0);
      this.weatherData.temp_max = x.temp_max.toFixed(0); 
-
-
-    /* this.city = x.updatedCity;
-     this.temp = x.temp.toFixed(0);
-     this.weatherID = x.weather.id;
-     this.temp_min = x.temp_min.toFixed(0);
-     this.temp_max = x.temp_max.toFixed(0); 
-    if (this.weatherDescription == this.weatherService.getWeatherType(this.weatherID)){
-    } else {
-      this.removeIcon();
-    } 
-     this.weatherDescription = this.weatherService.getWeatherType(this.weatherID);
-     console.log(this.weatherDescription)
-     this.activitiesWeatherDescription = this.weatherService.getActivities(this.temp, this.weatherDescription);
-     this.loading = false;*/
-
-
      if (this.weatherData.weatherDescription == this.weatherService.getWeatherType(this.weatherData.weatherID)){
-    } else {
+     } else {
       this.removeIcon();
-    } 
+     } 
      this.weatherData.weatherDescription = this.weatherService.getWeatherType(this.weatherData.weatherID);
-     console.log(this.weatherData.weatherDescription)
+     //console.log(this.weatherData.weatherDescription)
      this.weatherData.activitiesWeatherDescription = this.weatherService.getActivities(this.weatherData.temp, this.weatherData.weatherDescription);
      this.loading = false;
-
     },
     //error handling, if user input invalid, connected to HTML *ngIf 
       error => {
         console.log('error occured', error);
-
         this.weatherData.weatherDescription = "unknown";
         if (this.weatherData.weatherDescription == this.weatherService.getWeatherType(this.weatherData.weatherID)){
         } else {
           this.removeIcon();
         } 
-         this.weatherData.activitiesWeatherDescription = this.weatherService.getActivities(this.weatherData.temp, this.weatherData.weatherDescription);
-
-
-        /*this.weatherDescription = "unknown";
-        if (this.weatherDescription == this.weatherService.getWeatherType(this.weatherID)){
-        } else {
-          this.removeIcon();
-        } 
-         this.activitiesWeatherDescription = this.weatherService.getActivities(this.temp, this.weatherDescription);*/
+        this.weatherData.activitiesWeatherDescription = this.weatherService.getActivities(this.weatherData.temp, this.weatherData.weatherDescription);
         this.failed = true;
         this.loading = false; 
         this.reset();
@@ -139,12 +98,7 @@ textfieldIsVisible:boolean;
     this.weatherData.temp_max = 0; 
     this.weatherData.temp = 0;
     this.weatherData.weatherID = 0;
-    this.weatherData.weatherDescription = "unknown";
-   /* this.temp_min = 0;
-    this.temp_max = 0; 
-    this.temp = 0;
-    this.weatherID = 0;
-    this.weatherDescription = "unknown";*/
+   // this.weatherData.weatherDescription = "unknown";
   }
 
  
